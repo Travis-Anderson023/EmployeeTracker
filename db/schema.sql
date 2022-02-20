43e16338d93045116a785eced0bd2b5af8d31e64
Spotexx <tsanderson.023@gmail.com>
2022-02-20 11:37:25 -0500

    all choices work in menu

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE department (
 CREATE TABLE employee_role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary INT NOT NULL,
+    salary VARCHAR(30) NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
@@ -21,7 +21,7 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     FOREIGN KEY (role_id) REFERENCES employee_role(id) ON DELETE CASCADE,
     manager_id INT,
     FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
--- index.js
+++ index.js
@@ -15,65 +15,165 @@ const db = mysql.createConnection(
 //     if (err) throw err;
 //     console.log("Database created");
 // });
-const viewDepartments = () => {
+const viewDepartments = async () => {
+    db.query("SELECT * FROM department", function (err, result) {
+        if (err) throw err;
+        console.log('');
+        console.table(result);
+    });
 }
 
-const viewRoles = () => {
+const viewRoles = async () => {
+    db.query("SELECT * FROM employee_role", function (err, result) {
+        if (err) throw err;
+        console.log('');
+        console.table(result);
+    });
 }
 
-const viewEmployees = () => {
+const viewEmployees = async () => {
+    db.query("SELECT * FROM employee", function (err, result) {
+        if (err) throw err;
+        console.log('');
+        console.table(result);
+    });
 }
 
-const addDepartment = () => {
+const addDepartment = async () => {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: 'Enter Department Name',
+        }
+    ])
+    db.query(`INSERT INTO department (name) VALUES ('${answers.department}')`, function (err, result) {
+        if (err) throw err;
+        console.log('');
+        console.log(`${answers.department} added`);
+    });
 }
 
-const addRole = () => {
+const addRole = async () => {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'role',
+            message: 'Enter Role Name',
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Enter Salary',
+        },
+        {
+            type: 'number',
+            name: 'department_id',
+            message: 'Enter Department ID',
+        },
+    ])
+    db.query(`INSERT INTO employee_role (title, salary, department_id) VALUES ('${answers.role}', '${answers.salary}', '${answers.department_id}')`, function (err, result) {
+        if (err) throw err;
+        console.log(`${answers.role} added`);
+    });
 }
 
-const addEmployee = () => {
+const addEmployee = async () => {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'Enter First Name',
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'Enter Last Name',
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'Enter Role ID',
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'Enter Manager ID',
+        },
+    ])
+    //checks if answers.manager_id exists in employee table
+    let roleId = parseInt(answers.role_id);
+    let managerId = null;
+    db.query(`SELECT * FROM employee WHERE id = ${answers.manager_id}`, function (err, result) {
+        if (err) throw err;
+        if (result.length === 0) {
+            managerId = null;
+        } else {
+            managerId = null;
+        }
+    });
+
+    db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${answers.first_name}', '${answers.last_name}', ${roleId}, ${managerId})`, function (err, result) {
+        if (err) throw err;
+        console.log(`${answers.first_name} ${answers.last_name} added`);
+    });
+
 }
 
-const UpdateRole = () => {
+const updateRole = async () => {
+    console.log('Update Employee Role');
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employee_id',
+            message: 'Enter Employee ID',
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'Enter Role ID',
+        },
+    ])
+    db.query(`UPDATE employee SET role_id = '${answers.role_id}' WHERE id = '${answers.employee_id}'`, function (err, result) {
+        if (err) throw err;
+        console.log(`${answers.employee_id} updated`);
+    });
 }
 
-const menu = () => {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'menu',
-                message: 'Main Menu',
-                choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Exit'],
-            }
-        ])
-        .then(function (answer) {
-            switch (answer.menu) {
-                case 'View All Departments':
-                    viewDepartments();
-                    break;
-                case 'View All Roles':
-                    viewRoles();
-                    break;
-                case 'View All Employees':
-                    viewEmployees();
-                    break;
-                case 'Add Department':
-                    addDepartment();
-                    break;
-                case 'Add Role':
-                    addRole();
-                    break;
-                case 'Add Employee':
-                    addEmployee();
-                    break;
-                case 'Update EmployeeRole':
-                    UpdateRole();
-                    break;
-                case 'Exit':
-                    console.log('Goodbye');
-                    break;
-            }
-        });
+const menu = async () => {
+    const answers = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'Main Menu',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role'],
+        }
+    ])
+
+    switch (answers.menu) {
+        case 'View All Departments':
+            await viewDepartments();
+            break;
+        case 'View All Roles':
+            await viewRoles();
+            break;
+        case 'View All Employees':
+            await viewEmployees();
+            break;
+        case 'Add Department':
+            await addDepartment();
+            break;
+        case 'Add Role':
+            await addRole();
+            break;
+        case 'Add Employee':
+            await addEmployee();
+            break;
+        case 'Update Employee Role':
+            await updateRole();
+            break;
+    }
+    await menu();
 }
 
-
+menu();
