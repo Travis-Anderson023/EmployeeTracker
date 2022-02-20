c2af0190a0da54a56ca19ab4926fe5cf88d930f1
Spotexx <tsanderson.023@gmail.com>
2022-02-20 15:09:35 -0500

    connected manager name to employee

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const viewDepartments = async () => {
 
 const viewRoles = async () => {
     //presented with the job title, role id, the department that role belongs to, and the salary for that role
-    db.query("SELECT employee_role.title, employee_role.id, department.name, employee_role.salary FROM employee_role INNER JOIN department ON employee_role.department_id = department.id INNER JOIN employee ON employee_role.id = employee.role_id", function (err, result) {
+    db.query("SELECT employee_role.title, employee_role.id, department.name, employee_role.salary FROM employee_role LEFT JOIN department ON employee_role.department_id = department.id LEFT JOIN employee ON employee_role.id = employee.role_id", function (err, result) {
         if (err) throw err;
         console.log('');
         console.table(result);
@@ -34,7 +34,7 @@ const viewRoles = async () => {
 
 const viewEmployees = async () => {
     //presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-    db.query(`SELECT employee.id, employee.first_name, employee.last_name, employee_role.title, department.name, employee_role.salary, CONCAT(m.first_name, ' ', m.last_name) 'manager_title' FROM employee INNER JOIN employee_role ON employee.role_id = employee_role.id INNER JOIN department ON employee_role.department_id = department.id LEFT JOIN employee m ON (employee.manager_id = m.id)`, function (err, result) {
+    db.query(`SELECT employee.id, employee.first_name, employee.last_name, employee_role.title, department.name, employee_role.salary, CONCAT(m.first_name, ' ', m.last_name) 'manager_title' FROM employee LEFT JOIN employee_role ON employee.role_id = employee_role.id LEFT JOIN department ON employee_role.department_id = department.id LEFT JOIN employee m ON (employee.manager_id = m.id)`, function (err, result) {
         if (err) throw err;
         console.log('');
         console.table(result);
@@ -104,16 +104,20 @@ const addEmployee = async () => {
         },
     ])
     //checks if answers.manager_id exists in employee table
-    let roleId = parseInt(answers.role_id);
-    let managerId = null;
-    db.query(`SELECT * FROM employee WHERE id = ${answers.manager_id}`, function (err, result) {
-        if (err) throw err;
-        if (result.length === 0) {
-            managerId = null;
-        } else {
-            managerId = null;
-        }
-    });
+    let roleId;
+    let managerId;
+
+    if (answers.role_id === '') {
+        roleId = null;
+    } else {
+        roleId = answers.role_id;
+    }
+
+    if (answers.manager_id === '') {
+        managerId = null;
+    } else {
+        managerId = answers.manager_id;
+    }
 
     db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${answers.first_name}', '${answers.last_name}', ${roleId}, ${managerId})`, function (err, result) {
         if (err) throw err;
